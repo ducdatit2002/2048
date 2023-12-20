8bf06ea7b86f59df4c32d0f996c8fe18ae0c350b
Pham Vu Bao <vubao201@gmail.com>
2023-12-20 16:55:38 +0700

    add movesAvailable, tileMatchesAvailable and positionsEqual functions

--- controller/GameManager.js
+++ controller/GameManager.js
@@ -259,3 +259,39 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
       next: cell // Used to check if a merge is required
     };
   };
+
+  GameManager.prototype.movesAvailable = function () {
+    return this.grid.cellsAvailable() || this.tileMatchesAvailable();
+  };
+  
+  // Check for available matches between tiles (more expensive check)
+  GameManager.prototype.tileMatchesAvailable = function () {
+    var self = this;
+  
+    var tile;
+  
+    for (var x = 0; x < this.size; x++) {
+      for (var y = 0; y < this.size; y++) {
+        tile = this.grid.cellContent({ x: x, y: y });
+  
+        if (tile) {
+          for (var direction = 0; direction < 4; direction++) {
+            var vector = self.getVector(direction);
+            var cell   = { x: x + vector.x, y: y + vector.y };
+  
+            var other  = self.grid.cellContent(cell);
+  
+            if (other && other.value === tile.value) {
+              return true; // These two tiles can be merged
+            }
+          }
+        }
+      }
+    }
+  
+    return false;
+  };
+  
+  GameManager.prototype.positionsEqual = function (first, second) {
+    return first.x === second.x && first.y === second.y;
+  };
